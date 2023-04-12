90de4c1844431dffd3230283fc00054ccc3af56a
ChiaraChiorazzo <chiarachiorazzo1@gmail.com>
2023-04-12 21:44:05 +0200

    Login solo renderizado
    Login renderizado, con botón para crearse un nuevo usuario en caso de no tenerlo . Los forms no tienen ninguna validacion ni ningun manejo de datos por el momento y los botones aun no tienen funcionalidad

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,23 @@ import NavBar from './Components/NavBar/NavBar';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './Pages/Home'
 import Error from './Pages/Error'
-import Login from './Pages/Login'
+import Login from './Pages/Login/Login'
 import CreateProduct from './Pages/CreateProduct';
-function App() {
+import User from './Pages/NewUser/NewUser';
 
+function App() {
 
   return (
     <div className="App">
-
-      <NavBar />
+    
 
       {/* Routing  */}
       <Switch>
-        <Route path="/home" > <HomePage/></Route>
+        <Route path="/home" > <HomePage/> <NavBar/></Route>
         <Route path="/login"><Login/></Route>
-        <Route path="/createProduct"><CreateProduct/></Route>
-        <Route path="/*"><Error/></Route>
+        <Route path="/createProduct"><CreateProduct/> <NavBar/></Route>
+        <Route path="/newUser"> <User/> </Route>
+        <Route path="/*"><Error/><NavBar/></Route>
       </Switch>
     </div>
   );
