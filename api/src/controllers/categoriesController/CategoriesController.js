e65fe13f730ac37f2e53e4a29e48fb3a6fa6a1a7
Mariano Torres <marianxtorres@gmail.com>
2023-04-12 23:38:19 -0300

    route-categories-delete&update

--- api/src/controllers/categoriesController/CategoriesController.js
+++ api/src/controllers/categoriesController/CategoriesController.js
@@ -18,4 +18,18 @@ const postCategoriesDB = async (data) => {
   return { message: "Successfully created", newCategory };
 };
 
-module.exports = { getCategoriesFromDB, postCategoriesDB };
+const updateCategoryDB = async (name, id) => {
+  const updateCategory = await Category.update({ name: name }, { where: { id: id } })
+  
+  if(updateCategory !== 0) return {message: "Updated successfully"}
+  else return {message: "Error while updating"}
+}
+
+const deleteCategoryDB = async (id) => {
+  const categoryDeleted = await Category.destroy({ where: { id: id } })
+
+  if (categoryDeleted !== 0) return { message: "Successfully deleted" }
+  else return { message: "Error while deleting" }
+}
+
+module.exports = { getCategoriesFromDB, postCategoriesDB, updateCategoryDB, deleteCategoryDB };
---
+++ api/src/models/TypesModel.js
@@ -0,0 +1,22 @@
+const {DataTypes} = require('sequelize')
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        'type',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            }
+        },
+        {
+            timestamps: false,
+        }
+    )
+}
