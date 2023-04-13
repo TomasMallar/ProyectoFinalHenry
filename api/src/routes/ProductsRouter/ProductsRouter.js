3a7f5ef4e6dcb3d38a0f706da204b3a0067b2d18
Uktavh <119374218+Uktavh@users.noreply.github.com>
2023-04-12 22:07:52 -0300

    actualizacion rotuer Product
    se agrega la ruta post Product

--- api/src/routes/ProductsRouter/ProductsRouter.js
+++ api/src/routes/ProductsRouter/ProductsRouter.js
@@ -1,9 +1,11 @@
 const {getProductsHandler} = require ('../../handlers/productsHandler/getProductsHandler')
+const {postProductHandler} = require ('../../handlers/productsHandler/postProductHandler')
 
 const { Router } = require("express");
 
 const routerProducts = Router();
 
 routerProducts.get("/", getProductsHandler); // Traer chocolates, sea todos o por name
+pokemonsRouter.post('/', postProductHandler);
 
 module.exports = routerProducts;
