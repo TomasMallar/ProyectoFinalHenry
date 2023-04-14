774bd6ce30bfacbd7332bdba18214c6dcf6b95a1
ChiaraChiorazzo <chiarachiorazzo1@gmail.com>
2023-04-14 19:36:28 +0200

    SearchBar en SortingAndFiltering
    SearchBar en el archivo sorting and filtering, logica para que lo que el usuario busque se guarde dentro del estado queries en la propiedad name para luego pasarle a la ruta get todas las queries necesarias

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,40 +3,43 @@ import ProductCard from "../Components/ProductCard/ProductCard";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAllChocolates } from "../Redux/Actions/Actions";
+import SortingAndFiltering from "./sortingAndFiltering/sortingAndFiltering";
 
 const HomePage = (props) => {
-
+ 
   // let allProducts = props.allProducts
 
   const dispatch = useDispatch()
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getAllChocolates())
-}, [dispatch])
+  }, [dispatch])
 
-let allProducts = useSelector((state) => state.chocolates)
-
-
-
+  let allProducts = useSelector((state) => state.chocolates)
 
   return (
+
     <div>
-      Our products:
+      <div>
+        <SortingAndFiltering/>
+      </div>
       
-        {/* Map each product into a single card using map */}
-        {
-            allProducts?.map((product)=>{
-                return (
-                    <ProductCard
-                // as we are receiving info from the api with the "?" we make sure that we are getting the info and the app does not crash
-                       key={product?.id}
-                       id = {product.id} 
-                       name = {product?.name}
-                       image = {product?.image}
-                       price = {product?.price}
-                       score = {product?.score}
-                    />
-                )
+      <h4>Our products:</h4>
+
+      {/* Map each product into a single card using map */}
+      {
+        allProducts?.map((product) => {
+          return (
+            <ProductCard
+              // as we are receiving info from the api with the "?" we make sure that we are getting the info and the app does not crash
+              key={product?.id}
+              id={product.id}
+              name={product?.name}
+              image={product?.image}
+              price={product?.price}
+              score={product?.score}
+            />
+          )
         })}
     </div>
   )
