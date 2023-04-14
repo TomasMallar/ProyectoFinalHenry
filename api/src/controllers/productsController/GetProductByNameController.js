bab32d33ce83c289b8105683b84e836ff7259664
Uktavh <119374218+Uktavh@users.noreply.github.com>
2023-04-14 12:08:51 -0300

    Correccion de GetProductByName
    Correcion de Controller

--- api/src/controllers/productsController/GetProductByNameController.js
+++ api/src/controllers/productsController/GetProductByNameController.js
@@ -1,18 +1,26 @@
 const { Op } = require('sequelize');
-const {Product,Category} = require ('../../db');
+const {Product,Category,Type} = require ('../../db');
 const { cleanArrayProduct } = require('../../helpers/cleanArrayProduct');
 
 const getProductByName = async (name) => {
   
     const dBProduct = await Product.findAll({
         where: { name: { [Op.iLike]: `%${name}%` } },
-        include: {
+        include: [{
           model: Category,
           attributes: ["name"],
           through:{
             attributes: []
           }
-        }
+        },
+      {
+        model: Type,
+        attributes: ['name'],
+        through: {
+          attributes: [],
+        },
+      
+      }]
       });
       const dBfiltered = dBProduct.map(elem=>cleanArrayProduct(elem))
       
