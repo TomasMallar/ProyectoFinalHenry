60f0e0e3934bc8f7d3d55129bb0e273c31ba640d
FrancoChiorazzo <119111950+FrancoChiorazzo@users.noreply.github.com>
2023-04-14 08:29:04 -0300

    trae los datos del backend. Creacion de Data Management, Cards, Home
    Se agregó la tarjeta de producto como componente,
    Se mapearon en el home las tarjetas con todos los productos
    Se linkeó el backend con el frontend de manera local

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,39 @@
 import React from "react"
+import ProductCard from "../Components/ProductCard/ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllChocolates } from "../Redux/Actions/Actions";
 
-const HomePage = () => {
+const HomePage = (props) => {
+
+  let allProducts = props.allProducts
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getAllChocolates())
+}, [dispatch])
+
+
+
   return (
     <div>
-      <h1>Este es el Home</h1>
+      Cards
+      
+        {/* Map each product into a single card using map */}
+        {
+            allProducts?.map((product)=>{
+                return (
+                    <ProductCard
+                // as we are receiving info from the api with the "?" we make sure that we are getting the info and the app does not crash
+                       key={product?.id}
+                       id = {product.id} 
+                       name = {product?.name}
+                       image = {product?.image}
+                       price = {product?.price}
+                       score = {product?.score}
+                    />
+                )
+        })}
     </div>
   )
 };
--- client/src/Redux/Reducer/Reducer.js
+++ client/src/Redux/Reducer/Reducer.js
@@ -14,7 +14,6 @@ const initialState = {
     errorMessage:"",
     users:[]
 }
-
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_ALL_CHOCOLATES:
