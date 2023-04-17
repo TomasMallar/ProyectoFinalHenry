5db263e29685080f8f76135af52acd2d608e6e6c
Uktavh <119374218+Uktavh@users.noreply.github.com>
2023-04-17 17:43:02 -0300

    arreglo update

--- client/src/Redux/Actions/Actions.js
+++ client/src/Redux/Actions/Actions.js
@@ -200,7 +200,7 @@ export const GetAllIngredient = () => {
     export const PutProduct = (finalEditedProduct) => {
         return async function (dispatch) {
             try {
-                const resp = await axios.patch(`http://localhost:3001/products/update/${finalEditedProduct.id}`, finalEditedProduct)
+                const resp = await axios.put(`http://localhost:3001/products/update/${finalEditedProduct.id}`, finalEditedProduct)
                
                 const response = await axios(`http://localhost:3001/products/`)
         return dispatch({
