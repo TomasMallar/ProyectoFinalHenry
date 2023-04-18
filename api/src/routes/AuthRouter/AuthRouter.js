87609c303e25ca2e2ad50f1b704c1a0b8f330184
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-18 20:01:55 -0300

    Update AuthRouter.js
    Se comentó algunos scrore de google

--- api/src/routes/AuthRouter/AuthRouter.js
+++ api/src/routes/AuthRouter/AuthRouter.js
@@ -10,8 +10,8 @@ routerAuth.use("/google", passport.authenticate("auth-google", {
     scope: [
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/user.birthday.read",
-        "https://www.googleapis.com/auth/user.phonenumbers.read"
+        // "https://www.googleapis.com/auth/user.birthday.read",
+        // "https://www.googleapis.com/auth/user.phonenumbers.read"
     ],
     session: false,
 }), authGoogleHandler)
