7d7c3a6586f75f3c5b267d7dc5f7928bf3490ce8
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-18 20:00:50 -0300

    Update RolRouter.js
    Se agregaron rutas de roles | Se comentaron score de google

--- api/src/routes/RolesRouter/RolRouter.js
+++ api/src/routes/RolesRouter/RolRouter.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
-const { getRolesHandler, postRoleHandler } = require("../../handlers/rolesHandler/RolesHandler");
+const { getRolesHandler, postRoleHandler, putRolesHandler, deleteRolesHandler } = require("../../handlers/rolesHandler/RolesHandler");
 const validatePostRoles = require("../../middlewares/validateRoles/validatePostRoles")
 
 const routerRoles = Router();
 
 routerRoles.get("/", getRolesHandler);
 routerRoles.post("/create", validatePostRoles, postRoleHandler);
+routerRoles.put("/update/:id", putRolesHandler);
+routerRoles.delete("/delte", deleteRolesHandler);
 
 
 module.exports = routerRoles;
