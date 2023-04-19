dcdb51d3aa328ef1848ba038c0e29f485d6303d9
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-18 21:27:17 -0300

    Update RolRouter.js
    Se agregó a la ruta las validaciones de los roles

--- api/src/routes/RolesRouter/RolRouter.js
+++ api/src/routes/RolesRouter/RolRouter.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 const { getRolesHandler, postRoleHandler, putRolesHandler, deleteRolesHandler } = require("../../handlers/rolesHandler/RolesHandler");
-const validatePostRoles = require("../../middlewares/validateRoles/validatePostRoles")
+const validatePostRoles = require("../../middlewares/validateRoles/validatePostRoles");
+const validateJwt = require("../../middlewares/validateJwt/validateJwt");
+const validateRoleAdm = require("../../middlewares/validateRoleAdm/validateRolAdm")
 
 const routerRoles = Router();
 
-routerRoles.get("/", getRolesHandler);
+routerRoles.get("/", validateJwt, validateRoleAdm, getRolesHandler);
+// routerRoles.post("/create", validateJwt, validateRoleAdm, validatePostRoles, postRoleHandler);
 routerRoles.post("/create", validatePostRoles, postRoleHandler);
-routerRoles.put("/update/:id", putRolesHandler);
-routerRoles.delete("/delte", deleteRolesHandler);
+routerRoles.put("/update/:id", validateJwt, validateRoleAdm, putRolesHandler);
+routerRoles.delete("/delete/:id", validateJwt, validateRoleAdm, deleteRolesHandler);
 
 
 module.exports = routerRoles;
