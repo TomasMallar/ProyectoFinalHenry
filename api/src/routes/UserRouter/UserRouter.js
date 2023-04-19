bea58dc14777985b137a67b1ae866b6f76932fbe
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-19 14:28:43 -0300

    Update UserRouter.js
    Se agregó validación por id a la ruta put

--- api/src/routes/UserRouter/UserRouter.js
+++ api/src/routes/UserRouter/UserRouter.js
@@ -22,7 +22,7 @@ const routerUser = Router();
 routerUser.get("/", validateJwt, validateRoleAdm, getAllUserHandler);
 routerUser.get("/profile/:id", validateJwt, validateUserId, getOneUserHandler);
 routerUser.get("/searchuser", validateJwt, validateRoleAdm, getSearchUserHandler);
-routerUser.put("/update/:id", validateJwt, validatePutUser, updateUserHandler);
+routerUser.put("/update/:id", validateJwt, validateUserId, validatePutUser, updateUserHandler);
 routerUser.delete("/delete/:id", validateJwt, validateRoleAdm, deleteUserHandler);
 
 routerUser.post("/register", validatePostRegister, postNewtUserHandler);
