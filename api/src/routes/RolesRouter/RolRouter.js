e2a9a1d0cd456212ec5f6037c75c3f71f62662da
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-19 13:01:02 -0300

    Update RolRouter.js
    Se comento el delete roles

--- api/src/routes/RolesRouter/RolRouter.js
+++ api/src/routes/RolesRouter/RolRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getRolesHandler, postRoleHandler, putRolesHandler, deleteRolesHandler } = require("../../handlers/rolesHandler/RolesHandler");
+const { getRolesHandler, postRoleHandler, putRolesHandler } = require("../../handlers/rolesHandler/RolesHandler");
 const validatePostRoles = require("../../middlewares/validateRoles/validatePostRoles");
 const validateJwt = require("../../middlewares/validateJwt/validateJwt");
 const validateRoleAdm = require("../../middlewares/validateRoleAdm/validateRolAdm")
@@ -7,10 +7,12 @@ const validateRoleAdm = require("../../middlewares/validateRoleAdm/validateRolAd
 const routerRoles = Router();
 
 routerRoles.get("/", validateJwt, validateRoleAdm, getRolesHandler);
-// routerRoles.post("/create", validateJwt, validateRoleAdm, validatePostRoles, postRoleHandler);
-routerRoles.post("/create", validatePostRoles, postRoleHandler);
 routerRoles.put("/update/:id", validateJwt, validateRoleAdm, putRolesHandler);
-routerRoles.delete("/delete/:id", validateJwt, validateRoleAdm, deleteRolesHandler);
 
+// routerRoles.post("/create", validateJwt, validateRoleAdm, validatePostRoles, postRoleHandler);
+// routerRoles.post("/create", validatePostRoles, postRoleHandler);
+
+//Comento la ruta delete porque no tendría sentido eliminar un rol cuando se está trabajando con user y adm.
+// routerRoles.delete("/delete/:id", validateJwt, validateRoleAdm, deleteRolesHandler);
 
 module.exports = routerRoles;
