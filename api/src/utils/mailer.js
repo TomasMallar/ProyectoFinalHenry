56abd0e548ab6a829980b887274d5557f4cdaab6
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-20 21:11:47 -0300

    Update mailer.js
    Se borraron espacios de más

--- api/src/utils/mailer.js
+++ api/src/utils/mailer.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
-const { EMAIL_FROM, EMAIL_FROM_PASS, EMAIL_SERVICE} = process.env;
+const { EMAIL_FROM, EMAIL_FROM_PASS, EMAIL_SERVICE } = process.env;
 
 const transporter = nodemailer.createTransport({
   service: EMAIL_SERVICE,
   auth: {
-    user: EMAIL_FROM ,
+    user: EMAIL_FROM,
     pass: EMAIL_FROM_PASS,
   },
 });
