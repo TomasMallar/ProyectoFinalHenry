5fea65cc1548c9b344a42a3fa38e58d0b53ac663
Uktavh <119374218+Uktavh@users.noreply.github.com>
2023-04-22 20:43:08 -0300

    update
    union de venta por cryptos a ventas consolidadas en DB, se empieza a menejar el front de la pasarela de pagos ya creado el carrito de compras

--- api/src/routes/PaymentRouter/PaymentRouter.js
+++ api/src/routes/PaymentRouter/PaymentRouter.js
@@ -9,9 +9,6 @@ const routerPayment = Router();
 routerPayment.post('/create-order', createOrderHandler);
 routerPayment.post('/create-payment-preference', createPaymentPreferenceHandler);
 routerPayment.post('/handle-payment-notification', paymentNotificationHandler);
-
-
-
 routerPayment.post('/crypto-payment-notification', paymentCryptoNotificationHandler);
 // routerPayment.post('/handle-payment-notification', (req, res) => {
 //     console.log('Notificación recibida:', req.body);
