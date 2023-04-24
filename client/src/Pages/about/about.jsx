ecdfbb6e73eb39cc8e11a30fee23c400912d91bf
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 10:55:47 -0300

    Create about.jsx
    .

---
+++ client/src/Pages/about/about.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="bg-[url('https://images.unsplash.com/photo-1526081715791-7c538f86060e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')] bg-cover bg-center text-chocolate-blanco bg-chocolate-oscuro opacity-75 p-8">
+            <div className="shadow-md backdrop-blur-sm backdrop-brightness-50">
+
+                <h1 className="m-6 font-serif text-5xl">¡Bienvenido a nuestra tienda de chocolates en línea!</h1>
+                <p className="p-4 m-6 font-serif text-lg text-justify">
+                    En The Chocolate Hub, estamos apasionados por los chocolates y nos encanta compartir esa pasión contigo. Somos una empresa dedicada a ofrecerte los chocolates más deliciosos y exquisitos, elaborados con ingredientes de alta calidad y con un cuidado artesanal en cada detalle.
+                    <br />
+                    <br />
+                    Nuestra historia se remonta a 2023, cuando Ocatavio Torres Tomas comenzó a elaborar chocolates en su pequeño taller en Buenos Aires. Con el tiempo, su amor por el chocolate y su dedicación por la excelencia en sabor y presentación se ganaron el reconocimiento de los amantes del chocolate en toda la región.
+                    <br />
+                    <br />
+                    Hoy en día, The Chocolate Hub es una tienda de chocolates en línea reconocida a nivel nacional, pero seguimos manteniendo la misma atención al detalle y la calidad que nos ha hecho destacar desde el principio. Nuestro compromiso con la satisfacción del cliente es inquebrantable, y siempre nos esforzamos por ofrecer una experiencia de compra excepcional a cada uno de nuestros clientes.
+                    <br />
+                    <br />
+                    En nuestra tienda, encontrarás una amplia variedad de chocolates, desde los clásicos y tradicionales hasta los más innovadores y creativos. Nuestros maestros chocolateros trabajan arduamente para crear combinaciones de sabores únicas, texturas irresistibles y presentaciones hermosas que deleitarán tus sentidos.
+                    <br />
+                    <br />
+                    Además de nuestra pasión por los chocolates, también nos enorgullecemos de utilizar ingredientes de alta calidad y de apoyar prácticas sostenibles en nuestra producción. Nos preocupamos por el medio ambiente y por la comunidad, y trabajamos en estrecha colaboración con nuestros proveedores para garantizar que nuestros chocolates sean elaborados de manera responsable y ética.
+                    <br />
+                    <br />
+                    Gracias por visitar The Chocolate Hub y por unirte a nosotros en nuestra apasionante aventura chocolatera. ¡Esperamos que disfrutes de nuestros chocolates tanto como nosotros disfrutamos elaborarlos para ti!
+                    <br />
+                    <br />
+                    Si tienes alguna pregunta o comentario, no dudes en ponerte en contacto con nosotros. Estamos aquí para ayudarte en cualquier momento.
+                    <br />
+                    <br />
+                    ¡Gracias por tu apoyo!
+                    <br />
+                    <br />
+                    The Chocolate Hub
+                </p>
+            </div>
+        </div>
+    )
+}
