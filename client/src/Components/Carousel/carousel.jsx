111f95b4c43445bf7bd7bcfc72d4e9b6677365e1
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 10:55:57 -0300

    Update carousel.jsx

--- client/src/Components/Carousel/carousel.jsx
+++ client/src/Components/Carousel/carousel.jsx
@@ -1,22 +1,48 @@
 import { useSelector } from "react-redux";
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import DataManagement from "../../Components/DataManagement";
 import ProductCard from "../ProductCard/ProductCard";
 
 export default function FilterBar() {
 
-    let carouselItems = useSelector((state) => state.carousel.products)
+    const carouselRef = useRef(null);
+
+    let carouselItems = useSelector((state) => state.carousel.products);
+
+
+    const handleMoveLeft = () => {
+        carouselRef.current.scrollTo({
+            left: carouselRef.current.scrollLeft - carouselRef.current.offsetWidth,
+            behavior: "smooth"
+        });
+    };
+
+    const handleMoveRight = () => {
+        carouselRef.current.scrollTo({
+            left: carouselRef.current.scrollLeft + carouselRef.current.offsetWidth,
+            behavior: "smooth"
+        });
+    };
+
 
     return (
         <div className="relative p-5 overflow-hidden bg-chocolate-blanco">
-            <h1 className="m-6 font-serif text-5xl">NUEVA COLECCIÓN</h1>
-            <button className="absolute top-0 z-10 flex items-center justify-center w-[10vw] h-full border-none left-0 bg-gradient-to-r from-chocolate-blanco">
+            <h1 className="mb-6 font-serif text-5xl ml-14">
+                PRODUCTOS RECOMENDADOS
+                </h1>
+            <button className="absolute top-0 z-10 flex items-center justify-center w-[10vw] h-full border-none left-0 bg-gradient-to-r from-chocolate-blanco hover:from-chocolate-oscuro"
+            onClick={handleMoveLeft}
+            >
                 {'<<'}
             </button>
-            <button className="absolute top-0 z-10 flex items-center justify-center w-[10vw] h-full border-none right-0 bg-gradient-to-l from-chocolate-blanco">
+            <button className="absolute top-0 z-10 flex items-center justify-center w-[10vw] h-full border-none right-0 bg-gradient-to-l from-chocolate-blanco hover:from-chocolate-oscuro"
+            onClick={handleMoveRight}
+            >
                 {'>>'}
             </button>
-            <div className="px-[10vw] flex overflow-x-auto scroll-smooth">
+            <div className="px-[10vw] flex scroll-smooth overflow-x-hidden "
+                ref={carouselRef}
+            >
                 {
                     carouselItems?.map(p => {
                         const { id, name, image, categories, price, score } = p
