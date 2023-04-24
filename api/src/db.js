9c37e9b38ca4d61ad1200ea4201a439c18f6b309
ChiaraChiorazzo <chiarachiorazzo1@gmail.com>
2023-04-24 14:40:36 +0200

    Vuelta al E.Inicial
    Eliminé los cambios hechos para hacer el deploy, se encuentra todo como en el estado antes del merge por deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,27 +2,20 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 const sequelize = new Sequelize(
-  DB_DEPLOY,
-  {
-    define: {
-      paranoid: true, // Agregar esta línea
-      timestamps: true // Asegúrate de que 'timestamps' esté habilitado
-    },
-    //Agregué como variable de entorno el name de la db
-    logging: false,
-    native: false,
-
-    //DB conection with render (config)
-    dialectOptions:{
-      ssl:{
-        require:true
-      }
-    }
-  }
-);
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   {
+     define: {
+       paranoid: true, // Agregar esta línea
+       timestamps: true // Asegúrate de que 'timestamps' esté habilitado
+     },
+     //Agregué como variable de entorno el name de la db
+     logging: false,
+     native: false,
+   }
+ );
 
 const basename = path.basename(__filename);
 
