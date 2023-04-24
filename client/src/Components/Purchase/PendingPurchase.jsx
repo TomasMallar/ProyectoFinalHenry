f201b2bfc66c724c010f3709d0798014776225d1
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 10:56:25 -0300

    Update PendingPurchase.jsx
    .

--- client/src/Components/Purchase/PendingPurchase.jsx
+++ client/src/Components/Purchase/PendingPurchase.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const PendingPurchase = () => {
   return (
-    <div>
-      <h1>Compra pendiente</h1>
-      <p>Tu compra está pendiente de aprobación. Por favor, espera a que se complete el proceso.</p>
+    <div className="h-screen p-12 font-serif text-chocolate-oscuro bg-chocolate-blanco">
+      <div className='p-6 bg-chocolate-mantecol rounded-2xl'>
+      <h1 className='p-2 text-3xl'>Compra pendiente</h1>
+      <p className='p-2 text-xl'>Tu compra está pendiente de aprobación. Por favor, espera a que se complete el proceso.</p>
+      
+        <button className="p-4 text-xl font-bold shadow-sm h-fit shadow-chocolate-claro bg-chocolate-claro rounded-xl text-chocolate-oscuro hover:bg-chocolate-blanco">
+          <Link to="/home"> Volver a Home</Link>
+        </button>
+      </div>
     </div>
   );
 };
