c6a37fe20ff47a5b27bf1810d78817f0cce8a861
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 10:50:02 -0300

    Create MyShopping.jsx
    Se agregó MyShoppings

---
+++ client/src/Components/MyShopping/MyShopping.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyShopping = () => {
+  return (
+    <div>
+      <h2>My shopping</h2>
+      <p>Number of order: </p>
+      <h3>Products: </h3>
+    </div>
+  )
+}
+
+export default MyShopping;
