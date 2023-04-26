11c3468685f6a30d1edc987f0d7b6b9ad6aeb7eb
Agustin Minzoni <agustinminzoni@gmail.com>
2023-04-26 12:48:59 -0300

    PrevReactReveal

--- client/src/Components/Carousel/carousel.jsx
+++ client/src/Components/Carousel/carousel.jsx
@@ -1,62 +1,43 @@
 import { useSelector } from "react-redux";
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import DataManagement from "../../Components/DataManagement";
-import ProductCard from "../ProductCard/ProductCard";
+import CarouselCard from "../CarouselCard/carouselCard";
+import { Link } from "react-router-dom"
 
 export default function FilterBar() {
 
-    const carouselRef = useRef(null);
-
     let carouselItems = useSelector((state) => state.carousel.products);
 
-
-    const handleMoveLeft = () => {
-        carouselRef.current.scrollTo({
-            left: carouselRef.current.scrollLeft - carouselRef.current.offsetWidth,
-            behavior: "smooth"
-        });
-    };
-
-    const handleMoveRight = () => {
-        carouselRef.current.scrollTo({
-            left: carouselRef.current.scrollLeft + carouselRef.current.offsetWidth,
-            behavior: "smooth"
-        });
-    };
-
-
     return (
-        <div className="relative p-5 overflow-hidden bg-chocolate-blanco">
-            <h1 className="mb-6 font-serif text-5xl ml-14">
+        <div className="relative p-5 bg-chocolate-blanco">
+            <h1 className="py-6 font-serif text-4xl ml-14">
                 PRODUCTOS RECOMENDADOS
-                </h1>
-            <button className="absolute top-0 z-10 flex items-center justify-center w-[10vw] h-full border-none left-0 bg-gradient-to-r from-chocolate-blanco hover:from-chocolate-oscuro"
-            onClick={handleMoveLeft}
-            >
-                {'<<'}
-            </button>
-            <button className="absolute top-0 z-10 flex items-center justify-center w-[10vw] h-full border-none right-0 bg-gradient-to-l from-chocolate-blanco hover:from-chocolate-oscuro"
-            onClick={handleMoveRight}
-            >
-                {'>>'}
-            </button>
-            <div className="px-[10vw] flex scroll-smooth overflow-x-hidden "
-                ref={carouselRef}
-            >
+            </h1>
+            <div className="flex flex-wrap justify-around gap-3 py-6 items-evenly">
                 {
-                    carouselItems?.map(p => {
+                    carouselItems?.slice(0, 4)?.map(p => {
                         const { id, name, image, categories, price, score } = p
-                        return <ProductCard
-                            key={id}
-                            id={id}
-                            name={name}
-                            image={image}
-                            category={categories}
-                            price={price}
-                            score={score} />
+                        return (<Link to="/products">
+                            <CarouselCard
+                                key={id}
+                                id={id}
+                                name={name}
+                                image={image}
+                                category={categories}
+                                price={price}
+                                score={score} />
+                        </Link>
+                        )
                     })
                 }
             </div>
+
+            <button className="p-4 text-xl font-bold shadow-md h-fit shadow-chocolate-claro bg-chocolate-claro rounded-xl text-chocolate-oscuro hover:bg-chocolate-mantecol hover:shadow">
+                <Link to="/products">
+                Ver todos
+                </Link>
+            </button>
+
             <DataManagement />
         </div>)
 
--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -3,14 +3,33 @@ import FilterBar from "../../Components/Carousel/carousel"
 
 export default function Landing() {
     return (
-        <div>
-            <section>
-                <h1>Bienvenido a tu chocolateria de confinaza 🍫</h1>
+        <div className="px-4 font-serif bg-chocolate-blanco">
+            <section className=" bg-[url('https://images.unsplash.com/photo-1565071559227-20ab25b7685e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')] h-96 bg-fixed flex justify-center items-center">
+
+                <h1 className="text-4xl font-bold text-chocolate-blanco ">
+                    Bienvenido a tu chocolateria de confinaza 🍫
+                </h1>
             </section>
 
-            <FilterBar />
+            <section>
+                <FilterBar />
+            </section>
 
-            
+            <section className="flex items-center justify-center py-6">
+                <img src="https://www.cronista.com/files/image/465/465051/62a360d7e2ca7.jpg" alt="" className="w-[40%]"/>
+                <div className="w-[60]">
+                    <h3 className="py-4 text-3xl italic font-bold">
+                        90 años
+                    </h3>
+                    <p className="py-4 text-2xl italic">
+                        Nuestra chocolatería argentina cuenta con una trayectoria de más de 90 años en la industria del chocolate, siendo reconocidos por la calidad y artesanía de nuestros productos.
+                    </p>
+                    <button className="p-4 text-xl font-bold shadow-md h-fit shadow-chocolate-claro bg-chocolate-claro rounded-xl text-chocolate-oscuro hover:bg-chocolate-mantecol hover:shadow">
+                        PRUEBALOS YA!
+                    </button>
+                </div>
+            </section>
+
         </div>
     )
 }
