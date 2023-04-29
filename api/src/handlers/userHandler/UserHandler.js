2a141fc6248bf0ab9e8892c58448bb0a091495c2
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-28 21:37:45 -0300

    .

--- api/src/handlers/userHandler/UserHandler.js
+++ api/src/handlers/userHandler/UserHandler.js
@@ -1,117 +1,132 @@
-const { 
-    getAllUser, 
-    getOneUser,
-    getSearchUser,
-    postNewUser, 
-    postLoginUser, 
-    updateUser, 
-    deleteUser,
-    updatePassword 
-} = require("../../controllers/userController/UserController");
-const { sendWelcomeEmail } = require('../../controllers/emailControllers/emailNewUserController');
+const {
+  getAllUser,
+  getOneUser,
+  getSearchUser,
+  postNewUser,
+  postLoginUser,
+  updateUser,
+  deleteUser,
+  updatePassword,
+  getUserOrder,
+} = require('../../controllers/userController/UserController');
+const {
+  sendWelcomeEmail,
+} = require('../../controllers/emailControllers/emailNewUserController');
 
 const getAllUserHandler = async (req, res) => {
-    try {
-        const allUser = await getAllUser();
+  try {
+    const allUser = await getAllUser();
 
-        res.status(200).json(allUser);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+    res.status(200).json(allUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const getOneUserHandler = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const oneUser = await getOneUser(id);
+    const oneUser = await getOneUser(id);
 
-        res.status(200).json(oneUser);
-    } catch (error) {
-        res.status(400).json({ message: error.message})
-    }
-}
+    res.status(200).json(oneUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const getSearchUserHandler = async (req, res) => {
-    try {
-        const { name } = req.query;
+  try {
+    const { name } = req.query;
 
-        const usersSearch = await getSearchUser(name);
-    
-        res.status(200).json(usersSearch);
-    } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}
+    const usersSearch = await getSearchUser(name);
+
+    res.status(200).json(usersSearch);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const postNewtUserHandler = async (req, res) => {
-    try {
-        const { name, surname, password, mail, phone, date_of_birth } = req.body;
+  try {
+    const { name, surname, password, mail, phone, date_of_birth } = req.body;
 
-        const newUser = await postNewUser({
-            name, 
-            surname,
-            mail, 
-            phone, 
-            date_of_birth,
-            password
-        });
+    const newUser = await postNewUser({
+      name,
+      surname,
+      mail,
+      phone,
+      date_of_birth,
+      password,
+    });
 
-        // await sendWelcomeEmail(mail, name);
+    // await sendWelcomeEmail(mail, name);
 
-        res.status(200).json(newUser);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const postLoginUserHandler = async (req, res) => {
-    try {
-        const { mail, password } = req.body;
+  try {
+    const { mail, password } = req.body;
 
-        const userLogin = await postLoginUser({
-            mail,
-            password
-        }); 
+    const userLogin = await postLoginUser({
+      mail,
+      password,
+    });
 
-        res.status(200).json(userLogin);
-    } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}
+    res.status(200).json(userLogin);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const updateUserHandler = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const update = await updateUser(id, req.body);
+    const update = await updateUser(id, req.body);
 
-        res.status(200).json(update);
-    } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}
+    res.status(200).json(update);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const deleteUserHandler = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const userDelete = await deleteUser(id);
+    const userDelete = await deleteUser(id);
 
-        res.status(200).json(userDelete);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+    res.status(200).json(userDelete);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getUserOrderHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { page, limit } = req.query;
+    const hola = await getUserOrder(id, page, limit);
+
+    res.status(200).json(hola);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
 
 module.exports = {
-    getAllUserHandler,
-    getOneUserHandler,
-    getSearchUserHandler,
-    postNewtUserHandler,
-    postLoginUserHandler,
-    updateUserHandler,
-    deleteUserHandler,
-    // updatePasswordHanlder
-}
-
+  getAllUserHandler,
+  getOneUserHandler,
+  getSearchUserHandler,
+  postNewtUserHandler,
+  postLoginUserHandler,
+  updateUserHandler,
+  deleteUserHandler,
+  getUserOrderHandler,
+  // updatePasswordHanlder
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,53 +1,52 @@
-const { Router } = require("express");
-const routerProducts = require("./ProductsRouter/ProductsRouter");
-const routerCategories = require("./CategoryRouter/CategoryRouter");
-const routerTypes = require("./TypesRouter/TypesRouter");
-const routerUser = require("./UserRouter/UserRouter");
-const routerRoles = require("./RolesRouter/RolRouter");
-const routerFavoriteTypes = require("./FavTypesRouter/FavTypesRouter")
-const routerIngredient = require("./IngredientRouter/IngredientRouter")
-const routerScore = require('./PutScoreRouter/PutScoreRouter')
-const routerPay = require('./CompraRouter/mercadopago')
-const routerAuth = require("./AuthRouter/AuthRouter");
+const { Router } = require('express');
+const routerProducts = require('./ProductsRouter/ProductsRouter');
+const routerCategories = require('./CategoryRouter/CategoryRouter');
+const routerTypes = require('./TypesRouter/TypesRouter');
+const routerUser = require('./UserRouter/UserRouter');
+const routerRoles = require('./RolesRouter/RolRouter');
+const routerFavoriteTypes = require('./FavTypesRouter/FavTypesRouter');
+const routerIngredient = require('./IngredientRouter/IngredientRouter');
+const routerScore = require('./PutScoreRouter/PutScoreRouter');
+const routerPay = require('./CompraRouter/mercadopago');
+const routerAuth = require('./AuthRouter/AuthRouter');
 const routerEmail = require('./EmailRouter/EmailRouter');
-const routerPassword = require("./PasswordRouter/PasswordRouter")
-const routerPayment = require('./PaymentRouter/PaymentRouter')
-const routerMetric = require('./MetricRouter/MetricRouter')
-const routerComents = require('./ComentsRouter/ComentsRouter')
+const routerPassword = require('./PasswordRouter/PasswordRouter');
+const routerPayment = require('./PaymentRouter/PaymentRouter');
+const routerMetric = require('./MetricRouter/MetricRouter');
+const routerComents = require('./ComentsRouter/ComentsRouter');
 const router = Router();
 
-const passport = require("passport");
-require("../middlewares/authGoogle/authGoogle")
-
+const passport = require('passport');
+require('../middlewares/authGoogle/authGoogle');
 
 // Ruta para los chocolates
-router.use("/products", routerProducts);
-router.use("/categories", routerCategories);
-router.use("/types", routerTypes);
+router.use('/products', routerProducts);
+router.use('/categories', routerCategories);
+router.use('/types', routerTypes);
 
-router.use("/favtypes", routerFavoriteTypes)
-router.use("/ingredient", routerIngredient)
+router.use('/favtypes', routerFavoriteTypes);
+router.use('/ingredient', routerIngredient);
 
 //Rutas para usuarios y roles
-router.use("/email", routerEmail)
+router.use('/email', routerEmail);
 
-router.use("/users", routerUser);
-router.use("/roles", routerRoles);
+router.use('/users', routerUser);
+router.use('/roles', routerRoles);
 
-router.use("/score", routerScore)
+router.use('/score', routerScore);
 
-router.use('/pay', routerPay)
+router.use('/pay', routerPay);
 
-router.use('/payment', routerPayment)
+router.use('/payment', routerPayment);
 
-router.use('/metric',routerMetric)
+router.use('/metric', routerMetric);
 
 //Autenticación con google
-router.use("/auth", routerAuth);
+router.use('/auth', routerAuth);
 
 //Forgot password
-router.use("/password", routerPassword);
+router.use('/password', routerPassword);
 
-router.use('/coments', routerComents)
+router.use('/coments', routerComents);
 
 module.exports = router;
