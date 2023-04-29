3335e7edf31698fc7e29ef298f961a09c160ffb9
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-28 22:55:30 -0300

    Update Coments.jsx

--- client/src/Components/Coments/Coments.jsx
+++ client/src/Components/Coments/Coments.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 import style from './Coments.module.css'
 import jwt_decode from 'jwt-decode';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import axios from 'axios'
 
 const Coments = () => {
 
+    const history = useHistory()
+
     const [comments, setComments] = useState([])
+    console.log(comments);
     const [currentComment, setCurrentComment] = useState('')
     const [commentsDeleted, setCommentsDeleted] = useState(false)
     const [commentsUpdated, setCommentsUpdated] = useState(false)
@@ -14,13 +17,18 @@ const Coments = () => {
     const { id } = useParams()
 
     const token = sessionStorage.getItem('token');
-    const decodedToken = jwt_decode(token);
-    const userId = decodedToken.id;
-
+    let userId = null;
+    if (token) {
+        const decodedToken = jwt_decode(token);
+        userId = decodedToken.id;
+        console.log(userId);
+    }
+    console.log(userId);
     useEffect(() => {
         const getAllComents = async () => {
             const response = await axios.get(`http://localhost:3001/coments/${id}`)
             setComments(response.data)
+            setLatestComments(response.data.length)
         }
         getAllComents()
     }, [commentsDeleted, commentsUpdated])
@@ -30,13 +38,19 @@ const Coments = () => {
     }
 
     const postComment = async () => {
+        if (userId === null) {
+            alert('Debes iniciar sesión para poder comprar');
+            history.push('/login');
+            return;
+        }
+        if (currentComment.length === 0) return
         const newComment = {
             productId: id,
             userId: userId,
             content: currentComment
         }
         const response = await axios.post('http://localhost:3001/coments', newComment)
-        setComments([...comments, response.data])
+        setComments([response.data, ...comments])
     }
 
     const deleteComment = async (event) => {
@@ -66,32 +80,69 @@ const Coments = () => {
     const editCommentHandler = (comment) => {
         setEditComment({ id: comment.id, content: comment.content })
     }
+    const [cont, setCont] = useState(2)
+    const [latestComments, setLatestComments] = useState(null)
+    const pagesHandler = async () => {
+        const response = await axios.get(`http://localhost:3001/coments/${id}?page=${cont}`)
+        setCont(cont + 1)
+        setComments([...comments, ...response.data])
+        setLatestComments(response.data.length)
+    }
 
-        return (
-            <div className={style.container}>
-                <div className={style.containerComents}>
-                    <input type="text" placeholder='Escribe un comentario...' onChange={onChangeHandler} />
-                    <button onClick={postComment}>Publicar</button>
+    return (
+        <div className="flex flex-col items-center justify-center w-[100%] bg-chocolate-blanco rounded-xl">
+            <div className="w-full h-full">
+                <div className='flex items-center my-6 justify-evenly'>
+                    <textarea type="text" placeholder='Escribe un comentario...' onChange={onChangeHandler} maxlength="150" className="p-4 mb-2 text-base border-none shadow-sm bg-chocolate-mantecol w-[70%] h-28 rounded-2xl text-chocolate-oscuro shadow-chocolate-bombom cursor-text focus:outline-chocolate-bombom" />
+
+                    <button onClick={postComment} className='p-1 m-2 font-serif font-bold rounded-lg shadow-sm bg-chocolate-claro text-chocolate-oscuro shadow-chocolate-claro hover:bg-chocolate-blanco'>
+                        Publicar
+                    </button>
+                </div>
+
+                <div className='grid w-full h-full p-6 divide-y divide-current'>
                     {comments.map((comment, index) => (
-                        <div key={comment.id} className={style.comentarios}>
-                            <h3>{comment.name}</h3>
-                            <h4>{comment.createdAt.slice(0, 10)}</h4>
+                        <div key={comment.id} className="flex">
+                            <h3>
+                                {comment.name}
+                            </h3>
+                            <h4>
+                                {comment.createdAt.slice(0, 10)}
+                            </h4>
                             {editComment.id === comment.id ? (
-                                <form onSubmit={editCommentSubmit}>
-                                    <textarea value={editComment.content} onChange={(event) => setEditComment({ ...editComment, content: event.target.value })}/>
-                                    <button name={comment.id} type="submit">Guardar</button>
+                                <form onSubmit={editCommentSubmit} >
+                                    <textarea value={editComment.content} onChange={(event) => setEditComment({ ...editComment, content: event.target.value })} />
+                                    <button name={comment.id} type="submit">
+                                        Guardar
+                                    </button>
                                 </form>
                             ) : (
-                                <p>{comment.content}</p>
+                                <p>
+                                    {comment.content}
+                                </p>
                             )}
-                            <button onClick={() => editCommentHandler(comment)}>Editar</button>
-                            <button name={comment.id} onClick={deleteComment}>Eliminar</button>
+                            {comment.userId === userId && (
+                                <div>
+                                    <button onClick={() => editCommentHandler(comment)}>
+                                        Editar
+                                    </button>
+                                    <button name={comment.id} onClick={deleteComment}>
+                                        Eliminar
+                                    </button>
+                                </div>
+                            )
+                            }
+
                         </div>
                     ))}
                 </div>
-
+                {
+                    latestComments === 3 && <button className='m-10 ' onClick={pagesHandler}>Ver mas comentarios...</button>
+                }
             </div>
-        )
-    }
+
+        </div>
+    )
+}
 
 export default Coments
