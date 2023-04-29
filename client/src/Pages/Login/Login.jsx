1bd0736014f3347e7cdf08551269b19696479f10
Agustin Minzoni <agustinminzoni@gmail.com>
2023-04-29 12:38:59 -0300

    EstilosYanimaciones
    EstilosYanimaciones

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import { gapi } from "gapi-script"
 import axios from 'axios';
 import LoginButton from '../../Components/GoogleLogin/googleLogin.jsx';
 import jwtDecode from 'jwt-decode';
+import Fade from "react-reveal"
 
 import style from './Login.module.css'
 
@@ -68,45 +69,72 @@ export default function Login() {
         setPassword(e.target.value)
     }
     return (
-        <div>
-            {success ? (
-                <Redirect to="/home" />
-            ) : (
-                <div className={style.container}>
-                    <div className={style.formContainer}>
-                        <h2>Ingresa!</h2>
-                        <form className={style.inputContainer} onSubmit={handleSubmit}>
-                            <label htmlFor="username">Usuario</label>
-                            <input
-                                type="text"
-                                id="username"
-                                ref={userRef}
-                                autoComplete='off'
-                                onChange={handleUsername}
-                                value={user}
-                                placeholder="username"
-                                className={style.input}
-                            />
-                            <label htmlFor="password">Contraseña</label>
-                            <input
-                                type="password"
-                                id="password"
-                                onChange={handlePassword}
-                                value={password}
-                                placeholder="password"
-                                className={style.input}
-                            />
-                            <button className={style.btn}>Ingresar</button>
-                        </form>
-                        <p> ¿Aún no tienes una cuenta? <Link to="/newUser">Crear Cuenta</Link> </p>
-                        <p> Volver al <Link to="/home">Home</Link> </p>
-                        <LoginButton />
+        <div className="bg-[url('https://cdn.pixabay.com/photo/2017/08/01/02/10/dark-2562840_1280.jpg')] w-full h-screen pb-24 bg-chocolate-blanco bg-cover text-chocolate-oscuro">
+            <Fade left cascade>
+
+                {success ? (
+                    <Redirect to="/home" />
+                ) : (
+                    <div className="flex items-center justify-start w-full font-serif">
+
+                        <div className="flex flex-col items-center justify-center h-screen p-8 w-[400px] bg-chocolate-blanco">
+
+                            <h2 className='pb-5 text-2xl '>
+                                Ingresa!
+                            </h2>
+
+                            <form onSubmit={handleSubmit} className='my-6'>
+                                <label htmlFor="username">Usuario</label>
+                                <input
+                                    type="text"
+                                    id="username"
+                                    ref={userRef}
+                                    autoComplete='off'
+                                    onChange={handleUsername}
+                                    value={user}
+                                    placeholder="username"
+                                    className="flex flex-col justify-between p-2 mb-3 text-base border-none shadow-sm rounded-2xl bg-chocolate-mantecol w-44 text-chocolate-oscuro shadow-chocolate-bombom focus:outline focus:outline-chocolate-oscuro"
+                                />
+                                <label htmlFor="password">Contraseña</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    onChange={handlePassword}
+                                    value={password}
+                                    placeholder="password"
+                                    className="flex flex-col justify-between p-2 mb-3 text-base border-none shadow-sm rounded-2xl bg-chocolate-mantecol w-44 text-chocolate-oscuro shadow-chocolate-bombom focus:outline focus:outline-chocolate-oscuro"
+                                />
+
+                                <button className="p-1 m-2 font-serif font-bold rounded-lg shadow-sm bg-chocolate-claro text-chocolate-oscuro shadow-chocolate-claro hover:bg-chocolate-mantecol ">
+                                    Ingresar
+                                </button>
+                            </form>
+
+                            <p className='mb-2 text-chocolate-oscuro'>
+                                ¿Aún no tienes una cuenta? <Link to="/newUser" >
+                                    <span className='font-bold uppercase text-chocolate-bombom hover:decoration-chocolate-bombom hover:underline decoration-solid'>
+                                        Crear Cuenta
+                                    </span>
+                                </Link>
+                            </p>
+                            <p className='mb-2 text-chocolate-oscuro'>
+                                Volver al <Link to="/home">
+                                    <span className='font-bold uppercase text-chocolate-bombom hover:decoration-chocolate-bombom hover:underline decoration-solid'>
+                                        Home
+                                    </span>
+                                </Link>
+                            </p>
+
+                            <LoginButton />
+
+                        </div>
+
+                        <div className=" ml-80 w-[600px]">
+                            <h1 className='font-serif text-4xl text-start text-chocolate-blanco'>¡Bienvenid@ a The Chocolate hub! <br /> Inicia sesión para disfrutar de los mejores sabores y descubrir nuestras novedades. ¡Déjate tentar por el dulce placer del chocolate!</h1>
+                        </div>
                     </div>
-                    <div className={style.message}>
-                        <h1>¡Bienvenid@ a The Chocolate hub! <br /> Inicia sesión para disfrutar de los mejores sabores y descubrir nuestras novedades. ¡Déjate tentar por el dulce placer del chocolate!</h1>
-                    </div>
-                </div>
-            )}
+                )}
+            </Fade>
         </div>
     )
 }
