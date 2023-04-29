897ca439807bcd773e5733b432fb6d4be0746f3c
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-28 21:22:51 -0300

    Se agregó el myShopping | Se crearons los componentes de edit para el profile

---
+++ client/src/Components/EditProfileSurname/EditProfileSurname.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react';
+import style from './EditProfile.module.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { PutEditProfile } from '../../Redux/Actions/Actions';
+import { useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+const validate = (data) => {
+  const errors = {}; 
+  const regexName = /^([A-Za-zÁÉÍÓÚÑáéíóúñ]+[\s]?)+$/ //mayusculas minusculas acentos y nombres compuestos se aceptan 
+
+  if (!data.surname) {
+    errors.surname = 'Para enviar el dato debe estar completo'
+  } else if(!regexName.test(data.surname)) {
+    errors.surname = 'El apellido debe ser valido'
+  } 
+
+  return errors;
+}
+
+const EditProfileSurname = () => {
+  const dispatch = useDispatch();
+  const [editProfile, setEditProfile] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isProfileUpdated, setIsProfileUpdated] = useState(false);
+
+  const {surname} = JSON.parse(
+    sessionStorage.getItem('user')
+  );
+
+  const id = sessionStorage.getItem('id');
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    const property = e.target.name;
+
+    setErrors(
+      validate({
+        ...editProfile,
+        [property] : value,
+      })
+    )
+
+    setEditProfile({
+      ...editProfile,
+      [property]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(!errors.surname){
+      dispatch(PutEditProfile(id, editProfile));
+      alert('Perfil editado');
+      setIsProfileUpdated(true);
+      setErrors('');
+      setEditProfile({});
+    } else {
+      alert("Something went wrong. Please try again");
+      setIsProfileUpdated(false);
+    }
+  };
+
+  return isProfileUpdated ? (
+    <Redirect to='/myprofile' />
+  ) : (
+    <div className={style.container}>
+      <form onSubmit={handleSubmit} className={style.form}>
+        <div className={style.container}>
+          <div className={style.campos}>
+            <h3>Current surname</h3>
+            <p>{surname}</p>
+                  <label htmlFor='surname'>New surname</label>
+                  <input
+                    name='surname'
+                    value={editProfile.surname}
+                    onChange={handleInputChange}
+                    type='text'
+                  />
+          </div>
+        <p className={style.errors}>{errors.surname}</p>
+        <button type='submit' className={style.button}>
+          Guardar
+        </button>
+        <Link to='/myprofile' className={style.buttonCancelar}>
+          Cancelar
+        </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default EditProfileSurname
