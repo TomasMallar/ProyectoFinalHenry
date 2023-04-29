e1f65adaed31f71ac8eeb739ef53e35df384a3f1
Agustin Minzoni <agustinminzoni@gmail.com>
2023-04-28 22:34:35 -0300

    EstilizadoComponentes
    EstilizadoComponentes

--- client/src/Components/MenuProfile/MenuProfile.jsx
+++ client/src/Components/MenuProfile/MenuProfile.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import styles from "./MenuProfile.module.css"
+import Flip from 'react-reveal/Flip';
 
 const MenuProfile = () => {
   const [isNavActive, setIsNavActive] = useState(false);
@@ -12,23 +13,25 @@ const MenuProfile = () => {
     setIsNavActive(!isNavActive);
   };
 
-  const {name} = JSON.parse(sessionStorage.getItem("user"));
+  const { name } = JSON.parse(sessionStorage.getItem("user"));
 
   return (
-  <div className={styles.container}>
-    {isNavActive ? 
-      <button onClick={toggleNav} className={`text-2xl list-none text-red-oscuro no-underline pl-3.5  font-serif`}>{name}</button> 
-      :<button onClick={toggleNav} className={`text-2xl list-none text-chocolate-oscuro no-underline pl-3.5  font-serif`} >{name}</button>
-      }
-    <div className={`${styles.containerNav} ${isNavActive ? styles.active : ""}`}>
-      <div className={`${styles.buttonsNav} ${isNavActive ? styles.active : ""}`}>
-        <Link to="/myshopping">My Shopping</Link>
-        <Link to="/myprofile">My Profile</Link>
-      </div>
-    </div>
-  </div>
-      
-    
+    <div>
+      <Flip left cascade>
+        {isNavActive ?
+          <button onClick={toggleNav} className={`text-2xl list-none text-red-oscuro no-underline pl-3.5  font-serif`}>{name}</button>
+          : <button onClick={toggleNav} className={`text-2xl list-none text-chocolate-oscuro no-underline pl-3.5  font-serif`} >{name}</button>
+        }
+        <div className={`${styles.containerNav} ${isNavActive ? styles.active : ""}`}>
+          <div className={`${styles.buttonsNav} ${isNavActive ? styles.active : ""}`}>
+            <Link to="/myshopping">My Shopping</Link>
+            <Link to="/myprofile">My Profile</Link>
+          </div>
+        </div>
+      </Flip>
+    </div >
+
+
   )
 }
 
--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link, Redirect } from "react-router-dom";
-import {gapi} from "gapi-script"
+import { gapi } from "gapi-script"
 import axios from 'axios';
 import LoginButton from '../../Components/GoogleLogin/googleLogin.jsx';
 import jwtDecode from 'jwt-decode';
-
 
 import style from './Login.module.css'
 
@@ -17,103 +16,97 @@ export default function Login() {
     const [password, setPassword] = useState("")
     // const [errorMsg, setErrorMsg] = useState("")
     const [success, setSuccess] = useState(false)
-    
 
-    useEffect(()=>{
+
+    useEffect(() => {
         function start() {
             gapi.client.init({
                 client_Id: "552260192142-vtjdl7oc8tdsflnrq9ibuc07dd6aun0l.apps.googleusercontent.com",
                 scope: "",
             })
-            
+
         };
         gapi.load('client:auth2', start)
     })
 
-    const handleSubmit = async(e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = {mail:user, password};
+        const data = { mail: user, password };
         try {
-          const response = await axios.post("http://localhost:3001/users/login", data);
-          console.log(response.data);
-          if(response.data.user) {
-            sessionStorage.setItem("token", response.data.token);
-            sessionStorage.setItem("Name", response.data.user.name);
-            sessionStorage.setItem("user", JSON.stringify(response.data.user));
-            setUser("")
-            setPassword("")
-            setSuccess(true)
-            const decodedToken = jwtDecode(response.data.token);
-            const userRole = decodedToken.rol;
-            const id = decodedToken.id
-            sessionStorage.setItem('id', id)
-            // Guardar el rol en sessionStorage
-            sessionStorage.setItem("userRole", userRole);
-            console.log("ESTE ES EL ROL:",userRole);
-            window.location.reload();
-          }
+            const response = await axios.post("http://localhost:3001/users/login", data);
+            console.log(response.data);
+            if (response.data.user) {
+                sessionStorage.setItem("token", response.data.token);
+                sessionStorage.setItem("Name", response.data.user.name);
+                sessionStorage.setItem("user", JSON.stringify(response.data.user));
+                setUser("")
+                setPassword("")
+                setSuccess(true)
+                const decodedToken = jwtDecode(response.data.token);
+                const userRole = decodedToken.rol;
+                const id = decodedToken.id
+                sessionStorage.setItem('id', id)
+                // Guardar el rol en sessionStorage
+                sessionStorage.setItem("userRole", userRole);
+                console.log("ESTE ES EL ROL:", userRole);
+                window.location.reload();
+            }
         } catch (error) {
             alert(error.response.data.message)
-          console.error('There was a problem with the axios request:', error);
+            console.error('There was a problem with the axios request:', error);
         }
 
-      
+
     }
 
-    const handleUsername = (e) =>{
+    const handleUsername = (e) => {
         e.preventDefault();
         setUser(e.target.value)
     }
-    const handlePassword = (e) =>{
+    const handlePassword = (e) => {
         e.preventDefault();
         setPassword(e.target.value)
     }
     return (
-        
-
-        <>
-           { success? (
+        <div>
+            {success ? (
                 <Redirect to="/home" />
-            ):(
-        
-        <div className={style.container}>
-
-
-            <div className={style.formContainer}>
-                <h2>Ingresa!</h2>
-                <form className={style.inputContainer} onSubmit={handleSubmit}>
-                    <label htmlFor="username">Usuario</label>
-                    <input
-                        type="text"
-                        id="username"
-                        ref={userRef}
-                        autoComplete='off'
-                        onChange={handleUsername}
-                        value={user}
-                        placeholder="username"
-                        className={style.input}
-                    />
-                    <label htmlFor="password">Contraseña</label>
-                    <input
-                        type="password"
-                        id="password"
-                        onChange={handlePassword}
-                        value={password}
-                        placeholder="password"
-                        className={style.input}
-                    />
-                <button className={style.btn}>Ingresar</button>
-                </form>
-
-                <p> ¿Aún no tienes una cuenta? <Link to="/newUser">Crear Cuenta</Link> </p>
-                <p> Volver al <Link to="/home">Home</Link> </p>
-                <LoginButton />
-            </div>
-
-            <div className={style.message}>
-                <h1>¡Bienvenid@ a The Chocolate hub! <br /> Inicia sesión para disfrutar de los mejores sabores y descubrir nuestras novedades. ¡Déjate tentar por el dulce placer del chocolate!</h1>
-            </div>
+            ) : (
+                <div className={style.container}>
+                    <div className={style.formContainer}>
+                        <h2>Ingresa!</h2>
+                        <form className={style.inputContainer} onSubmit={handleSubmit}>
+                            <label htmlFor="username">Usuario</label>
+                            <input
+                                type="text"
+                                id="username"
+                                ref={userRef}
+                                autoComplete='off'
+                                onChange={handleUsername}
+                                value={user}
+                                placeholder="username"
+                                className={style.input}
+                            />
+                            <label htmlFor="password">Contraseña</label>
+                            <input
+                                type="password"
+                                id="password"
+                                onChange={handlePassword}
+                                value={password}
+                                placeholder="password"
+                                className={style.input}
+                            />
+                            <button className={style.btn}>Ingresar</button>
+                        </form>
+                        <p> ¿Aún no tienes una cuenta? <Link to="/newUser">Crear Cuenta</Link> </p>
+                        <p> Volver al <Link to="/home">Home</Link> </p>
+                        <LoginButton />
+                    </div>
+                    <div className={style.message}>
+                        <h1>¡Bienvenid@ a The Chocolate hub! <br /> Inicia sesión para disfrutar de los mejores sabores y descubrir nuestras novedades. ¡Déjate tentar por el dulce placer del chocolate!</h1>
+                    </div>
+                </div>
+            )}
         </div>
-    )}</>
     )
 }
