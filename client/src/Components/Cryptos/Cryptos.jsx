8ff78cd4a1f713665c392a9430867e5902ddb33c
Mariano Torres <marianxtorres@gmail.com>
2023-05-02 14:55:40 -0300

    cryptos

--- client/src/Components/Cryptos/Cryptos.jsx
+++ client/src/Components/Cryptos/Cryptos.jsx
@@ -9,11 +9,10 @@ const Crypto = (order) => {
     const [responseMessage, setResponseMessage] = useState('')
     const startPayment = async ({ ether, addr }) => {
         try {
-
             if (!window.ethereum) {
                 throw new Error("No crypto wallet found. Please install it.");
             }
-
+            
             await window.ethereum.send("eth_requestAccounts");
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             const signer = provider.getSigner();
@@ -22,14 +21,17 @@ const Crypto = (order) => {
                 to: addr,
                 value: ethers.utils.parseEther(ether),
             });
-
-
+            
+            console.log('hola');
+            console.log("SOY TX",tx);
+            console.log("SOY ORDER",order.order);
+            console.log("SOY ETHER",ether);
             const body = {
                 hash: tx.hash,
                 amount: ether,
                 orderId: order.order
             }
-            console.log(body);
+            console.log("SOY BODY",body);
             await axios.post('http://localhost:3001/payment/crypto-payment-notification', body)
             history.push('/purchase/approved');
 
@@ -41,25 +43,39 @@ const Crypto = (order) => {
         }
     };
 
-    const pesosAEther = () => {
+    const pesosAEther = async () => {
+        const storedCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+        const fetchCartItemsInfo = async () => {
+            const cartItemsInfo = await Promise.all(storedCartItems.map(async (cartItem) => {
+              const response = await fetch(`http://localhost:3001/products/${cartItem.id}`);
+              const product = await response.json();
+              return { ...product, quantity: cartItem.quantity };
+            }));
+            return cartItemsInfo;
+          }
         const items = JSON.parse(localStorage.getItem('cartItems'))
-        const totalPrice = items.reduce((acumulador, currentValue) => {
-            return acumulador + currentValue.price
-        }, 0)
-        console.log(totalPrice);
+        console.log("SOY ITEMS",items);
+        const items2 = await fetchCartItemsInfo(items)
+        console.log("SOY ITEMS2",items2);
+
+        const totalPrice = items2.reduce((accumulator, currentValue) => {
+            const productPrice = parseFloat(currentValue.price) * parseFloat(currentValue.quantity)
+            return accumulator + productPrice
+          }, 0)
+        console.log("SOY TOTAL PRICE",totalPrice);
 
         const cambio = 0.0000025
         let ether = totalPrice * cambio
-        console.log(ether.toString().slice(0, 7));
+        console.log("ether",ether.toString().slice(0, 7));
         return ether.toString().slice(0, 7)
     }
 
     const handlePayment = async () => {
-        const ether = pesosAEther()
+        const ether = await pesosAEther()
         const addr = "0x0C16F41d6e190CdA2E3A002FD518AC0B5367C3D9";
         const tx = await startPayment({ ether, addr });
 
-        console.log(tx, responseMessage);
+        console.log("RESPONSE",tx, responseMessage);
     };
 
     return (
