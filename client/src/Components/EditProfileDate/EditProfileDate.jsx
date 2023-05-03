9905914d87412a64462f9c2fe79aa1631f46d05a
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-03 10:11:18 -0300

    Se corrigieron bugs cuando de editaba el perfil

--- client/src/Components/EditProfileDate/EditProfileDate.jsx
+++ client/src/Components/EditProfileDate/EditProfileDate.jsx
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 import { PutEditProfile } from '../../Redux/Actions/Actions';
 import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const validate = (data) => {
-  const errors = {}; 
+  const errors = {};
 
   if (!data.date_of_birth) {
-    errors.date_of_birth = 'Para enviar el dato debe estar completo'
+    errors.date_of_birth = 'Para enviar el dato debe estar completo';
   }
-console.log(data.date_of_birth);
+  console.log(data.date_of_birth);
   return errors;
-}
+};
 
 const EditProfileDate = () => {
   const dispatch = useDispatch();
@@ -22,9 +23,7 @@ const EditProfileDate = () => {
   const [errors, setErrors] = useState({});
   const [isProfileUpdated, setIsProfileUpdated] = useState(false);
 
-  const {date_of_birth} = JSON.parse(
-    localStorage.getItem('user')
-  );
+  const { date_of_birth } = JSON.parse(localStorage.getItem('user'));
 
   const id = localStorage.getItem('id');
 
@@ -35,9 +34,9 @@ const EditProfileDate = () => {
     setErrors(
       validate({
         ...editProfile,
-        [property] : value,
+        [property]: value,
       })
-    )
+    );
 
     setEditProfile({
       ...editProfile,
@@ -45,28 +44,43 @@ const EditProfileDate = () => {
     });
   };
 
-  useEffect(() => {
-    const userData = JSON.parse(localStorage.getItem('user'));
-    const updatedUserData = { ...userData, ...editProfile };
-    localStorage.setItem('user', JSON.stringify(updatedUserData));
-  }, [editProfile]);
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (Object.keys(editProfile).length === 0) {
-      alert('Por favor, complete los campos');
+      Swal.fire({
+        icon: 'warning',
+        title: 'Por favor, complete los campos',
+        showConfirmButton: false,
+        timer: 2000,
+      });
       return;
     }
-    
-    if(!errors.date_of_birth){
+
+    if (!errors.date_of_birth) {
       dispatch(PutEditProfile(id, editProfile));
-      alert('Perfil editado');
-      setIsProfileUpdated(true);
-      setErrors('');
-      setEditProfile({});
+
+      const userData = JSON.parse(localStorage.getItem('user'));
+      const updatedUserData = { ...userData, ...editProfile };
+      localStorage.setItem('user', JSON.stringify(updatedUserData));
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Perfil editado',
+        showConfirmButton: false,
+        timer: 2000,
+      }).then(() => {
+        setIsProfileUpdated(true);
+        setErrors('');
+        setEditProfile({});
+      });
     } else {
-      alert("Something went wrong. Please try again");
+      Swal.fire({
+        icon: 'error',
+        title: 'Algo salió mal, por favor intenta nuevamente',
+        showConfirmButton: false,
+        timer: 2000,
+      });
       setIsProfileUpdated(false);
     }
   };
@@ -75,28 +89,33 @@ const EditProfileDate = () => {
     <Redirect to='/myprofile' />
   ) : (
     <div className={style.container}>
+      <h3 className={style.title}>
+        Edita aquí tu fecha de nacimiento por favor:
+      </h3>
       <form onSubmit={handleSubmit} className={style.form}>
         <div className={style.container}>
           <div className={style.campos}>
-                  <label htmlFor='date_of_birth'>New date of birthday</label>
-                  <input
-                    name='date_of_birth'
-                    value={editProfile.date_of_birth}
-                    onChange={handleInputChange}
-                    type='date'
-                  />
+            <label htmlFor='date_of_birth'>New date of birthday</label>
+            <input
+              name='date_of_birth'
+              value={editProfile.date_of_birth}
+              onChange={handleInputChange}
+              type='date'
+            />
           </div>
-        <p className={style.errors}>{errors.date_of_birth}</p>
-        <button type='submit' className={style.button}>
-          Guardar
-        </button>
-        <Link to='/myprofile' className={style.buttonCancelar}>
-          Cancelar
-        </Link>
+          <p className={style.errors}>{errors.date_of_birth}</p>
+          <div className={style.divButton}>
+            <button type='submit' className={style.button}>
+              Guardar
+            </button>
+            <Link to='/myprofile' className={style.buttonCancelar}>
+              Cancelar
+            </Link>
+          </div>
         </div>
       </form>
     </div>
   );
-}
+};
 
-export default EditProfileDate
+export default EditProfileDate;
